ee97741b854e250d6744ce1eebd49b8919676131
chriskontsis <cak0927@gmail.com>
2022-06-19 09:58:24 -0700

    3 hours of trying to read user input

--- script.js
+++ script.js
@@ -1,15 +1,21 @@
 let ticker = "BTC";
-let itemId;
+let tickerId;
+let dataArr;
 
+/* this function gets the user input from the forum and gets the correct id for the ticker specified in the input
+in order to use id to get the information for the specified ticker */
 async function formdata() {
    const tickerInfo = await document.getElementById("ticker-txt").value;
    event.preventDefault();
    const response = await fetch('https://api.coinpaprika.com/v1/tickers');
    const json = await response.json();
-   const userInputTickerId = await findTicker(json, tickerInfo);
-   console.log(userInputTickerId);
+   tickerId = await findTicker(json, tickerInfo); 
+   const data = await cryptoPage(tickerId);
+   console.log(data);
+  
 }
 
+// loops throguh json to find the id.
 function findTicker(json, tickerInfo) {
     for(let key of json) {
        if(key.symbol == tickerInfo) {
@@ -18,3 +24,10 @@ function findTicker(json, tickerInfo) {
     }
 }
 
+
+async function cryptoPage() {
+    let fetcher = await fetch('https://api.coinpaprika.com/v1/tickers/' + tickerId);
+    let json = fetcher.json();
+    dataArr = await parseJSON(json);
+    return dataArr;
+}
