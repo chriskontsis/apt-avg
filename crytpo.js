24b3dea912400499fdfd2c51bc0044f161583fde
chriskontsis <cak0927@gmail.com>
2022-06-21 09:27:45 -0700

    css styling

--- crytpo.js
+++ crytpo.js
@@ -4,12 +4,12 @@ jsArray = JSON.parse(sessionStorage.getItem("jsArray"));
 function setInfo() {
     document.getElementById("coinName").innerHTML = jsArray.name;
     document.getElementById("symbol").innerHTML = jsArray.symbol;
-    document.getElementById("price").innerHTML = jsArray.priceUSD;
-    document.getElementById("volume_24h").innerHTML = jsArray.volume_24h;
-    document.getElementById("mkt_cap").innerHTML = jsArray.mkt_cap;
-    document.getElementById("pctchange1hr").innerHTML = jsArray.pctchange1hr;
-    document.getElementById("pctchange24hr").innerHTML = jsArray.pctchange24hr;
-    document.getElementById("pctchange30day").innerHTML = jsArray.pctchange30day;
-    document.getElementById("pctchange1yr").innerHTML = jsArray.pctchange1yr;
+    document.getElementById("price").innerHTML = "price: " + jsArray.priceUSD;
+    document.getElementById("volume_24h").innerHTML = "24h Vol: " + jsArray.volume_24h;
+    document.getElementById("mkt_cap").innerHTML = "Mkt Cap " + jsArray.mkt_cap;
+    document.getElementById("pctchange1hr").innerHTML = "1h " + jsArray.pctchange1hr + "% ";
+    document.getElementById("pctchange24hr").innerHTML = "24h " +jsArray.pctchange24hr + "% ";
+    document.getElementById("pctchange30day").innerHTML = "30d " + jsArray.pctchange30day + "% ";
+    document.getElementById("pctchange1yr").innerHTML = "1yr " + jsArray.pctchange1yr + "% ";
 
 }
